40d8fec5603d5cf5d31511a73622add213c33d33
Carson Sievert <cpsievert1@gmail.com>
2017-01-05 17:34:17 -0600

    some experiments

---
+++ 03-households.R
@@ -0,0 +1,18 @@
+
+
+# Manually downloaded from
+# http://www.bcstats.gov.bc.ca/StatisticsBySubject/Demography/Households.aspx
+# Select "regional district" in dropdown, shift-click to select all regions/years
+
+path <- "data/households.csv"
+
+# read it in to clean it up a bit
+houses <- readr::read_csv(path)[, -1]
+houses <- setNames(houses, c("region", "year", "houses"))
+
+# TODO: are some of these locations outside BC? And some inside?
+houses %>%
+  mutate(isBC = region == "British Columbia") %>%
+  group_by(isBC, year) %>%
+  summarise(n = sum(houses)) %>%
+  arrange(year)
---
+++ 04-hpi.R
@@ -0,0 +1,3 @@
+# manually downloaded from http://www.housepriceindex.ca/
+path <- "data/hpi_12-29-2016.xls"
+dat <- readr::read_csv(path)
